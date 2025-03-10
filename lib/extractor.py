9158e7202cb86d784d141dfea5d3b7ba8a6b17d9
Aaron Myatt <aaronmyatt@gmail.com>
2025-03-10 18:02:03 +0800

    generate anki flashcards from directory

--- lib/extractor.py
+++ lib/extractor.py
@@ -1,2 +1,3 @@
-from extract_language_feature import analyze_file
+from directory_tree_flash_cards import generate_flashcards_for_directory,save_flashcards_to_files
 
+print(generate_flashcards_for_directory('.', ['dnotes']))
